2f4fdfec2ebb0c334e1836a006983f99ea32240f
Soojung Kwak <gsj2464@gmail.com>
2021-06-23 08:09:51 +0900

    preAuthorize

--- src/main/java/com/example/security/controller/StudentManagementController.java
+++ src/main/java/com/example/security/controller/StudentManagementController.java
@@ -1,6 +1,7 @@
 package com.example.security.controller;
 
 import com.example.security.dto.Student;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,21 +18,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable int studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable int studentId, @RequestBody Student student) {
         System.out.println(String.format("%s %s", studentId, student));
     }
