8ad8f5fcc8d1cbe236fd08763a6576a4adfd82e4
Soojung Kwak <gsj2464@gmail.com>
2021-06-23 09:14:00 +0900

    form based authentication

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -33,15 +33,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true);
     }
 
     @Override
